571d2781b7cfd809b0d78bf436ed600636b21448
Daniil Alexandrov <daneelalexandrov@gmail.com>
2025-01-23 11:02:55 +0800

    Test2

--- resources/views/sign.blade.php
+++ resources/views/sign.blade.php
@@ -5,7 +5,7 @@
         <div class="col-md-8">
 
             <div class="card">
-                <div class="card-header">Register</div>
+                <div class="card-header">Reg</div>
                 <div class="card-body">
                     <form action="{{ route('store') }}" method="post">
                         @csrf
